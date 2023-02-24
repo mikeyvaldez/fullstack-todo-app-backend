2e4e9f9f5c51fe0c26ad3ebb9f61b81374089d00
Michael Valdez <44276249+mikeyvaldez@users.noreply.github.com>
2023-02-24 11:53:37 -0800

    Add files via upload

---
+++ routes/ToDoRoute.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+
+const { getToDo, saveToDo, deleteToDo, updateToDo } = require("../controllers/ToDoController");
+
+const router = Router();
+
+router.get("/", getToDo);
+
+router.post("/save", saveToDo);
+
+router.put("/update", updateToDo);
+
+router.put("/delete", deleteToDo);
+
+module.exports = router;
